6dbfe131855ea9164f2f392cd1eaa500c918a730
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-03-16 08:28:53 +0900

    오브젝트 풀링 코딩중(현재  오브젝트 저장만 했음)

--- ProjectOfClickerVisual/Assets/Script/PoolManager.cs
+++ ProjectOfClickerVisual/Assets/Script/PoolManager.cs
@@ -1,13 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ObjectType
+{
+    clickEffect,
+    moneyText
+}
+
+[System.Serializable]
 public class PoolInfo
 {
+    public ObjectType type;
+    public int amount = 0;
+    public GameObject Prfab = null;
+    public GameObject container;
 
+    [HideInInspector]
+    public List<GameObject> pool = new List<GameObject>();
 }
 
 public class PoolManager : MonoBehaviour
 {
+    [SerializeField]
+    List<PoolInfo> listofPool;
 
+    private Vector3 defaltPos = new Vector3(-100, -100, -100);
+
+    private void Start()
+    {
+        for(int i =0; i<listofPool.Count; i++)
+        {
+            FillPool(listofPool[i]);
+        }
+    }
+    
+    public void FillPool(PoolInfo poolInfo)
+    {
+        for(int i = 0; i<poolInfo.amount; i++)
+        {
+            GameObject objectInst = null;
+            objectInst = Instantiate(poolInfo.Prfab, poolInfo.container.transform);
+            objectInst.gameObject.SetActive(false);
+            objectInst.transform.position = defaltPos;
+            poolInfo.pool.Add(objectInst);
+        }
+    }
+
+    //public GameObject GetPoolObject(ObjectType type)
+    //{
+
+    //}
 }
