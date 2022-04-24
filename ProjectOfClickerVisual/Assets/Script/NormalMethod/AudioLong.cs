cf80b9dbf86d36b28e9f9379c551b37a9a74b0e9
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-04-25 07:52:30 +0900

    오디오 플레이어 UI 기능 추가 완료

--- ProjectOfClickerVisual/Assets/Script/NormalMethod/AudioLong.cs
+++ ProjectOfClickerVisual/Assets/Script/NormalMethod/AudioLong.cs
@@ -5,5 +5,30 @@ using UnityEngine;
 
 public class AudioLong : MonoBehaviour
 {
+    [SerializeField]
+    private AudioSource audioRed;
+    [SerializeField]
+    private Slider AudioScanslider;
 
+    private void Start()
+    {
+        StartCoroutine(StartAudio());
+    }
+    
+    IEnumerator StartAudio()
+    {
+        yield return new WaitForEndOfFrame();
+        AudioScanslider.maxValue = audioRed.clip.length;
+        AudioScanslider.value = 0;
+    }
+
+    private void Update()
+    {
+        AudioUpdate();
+    }
+
+    private void AudioUpdate()
+    {
+        AudioScanslider.value = audioRed.time;
+    }
 }
