e07b61d58fdb14062c26755720b9905020e65042
DESKTOP-OHD6DA9\USER <cjfwls05324@naver.com>
2022-06-15 11:03:45 +0900

    진화 효과 추가중

--- ProjectOfClickerVisual/Assets/Script/Manager/RevolutionManager.cs
+++ ProjectOfClickerVisual/Assets/Script/Manager/RevolutionManager.cs
@@ -8,7 +8,7 @@ using DG.Tweening;
 public class RevolutionManager : MonoBehaviour
 {
     [SerializeField]
-    private Image FoxObject;
+    private Image[] FoxObject;
 
     [SerializeField]
     private SpriteRenderer FadeSprite;
@@ -23,11 +23,30 @@ public class RevolutionManager : MonoBehaviour
         gameManager = GetComponent<GameManager>();
     }
 
-    void SetRevolution()
+    public void ClickRevol()
+    {
+        StartCoroutine(SetRevolution());
+    }
+
+    IEnumerator SetRevolution()
     {
         if (gameManager.FoxLevel > 3)
         {
             FadeSprite.DOFade(1f, 1f);
+            yield return new WaitForSeconds(1f);
+            FoxObject[GameManager.Inst.FoxLevel].gameObject.SetActive(true);
+            FoxObject[GameManager.Inst.FoxLevel - 1].gameObject.SetActive(true);
+            for(int i = 0; i<3; i++)
+            {
+                FoxObject[GameManager.Inst.FoxLevel-1].transform.DOScale(new Vector3(FoxObject[GameManager.Inst.FoxLevel-1].transform.localScale.x * 1.7f, FoxObject[GameManager.Inst.FoxLevel - 1].transform.localScale.y * 1.7f, FoxObject[GameManager.Inst.FoxLevel - 1].transform.localScale.z * 1.7f), 0.4f);
+                yield return new WaitForSeconds(0.4f);
+            }
+            FoxObject[GameManager.Inst.FoxLevel - 1].DOColor(Color.white,2f);
+            FoxObject[GameManager.Inst.FoxLevel].DOColor(Color.white, 2f);
+            yield return new WaitForSeconds(2f);
+            FoxObject[GameManager.Inst.FoxLevel].DOFade(1, 1f);
+            yield return new WaitForSeconds(1f);
+            GameManager.Inst.FoxLevel++;
         }
     }
 
